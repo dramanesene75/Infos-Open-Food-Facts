3cc6836e82f9f00f8dccae254480576d81e21282
Dramane Sene <dramane.sene@viseo.com>
2019-07-01 16:34:06 +0200

    swipe picture

--- lib/ProductDetails.dart
+++ lib/ProductDetails.dart
@@ -52,15 +52,15 @@ class Post {
 
   factory Post.fromJsonByBarCode(Map<String, dynamic> json) {
     return Post(
-      productName: json ['status'] ==1&& json['product']['product_name'] != '' ?json['product']['product_name']:'',
-      ingredients: json ['status']==1 && json['product']['ingredients_text'] != '' ? json['product']['ingredients_text']:'Ingredients Unknown',
-      allergens: json ['status']==1 && json['product']['allergens'] != '' ? json['product']['allergens']:'No allergens',
-      traces:  json ['status']==1 && json['product']['traces'] != '' ? json['product']['traces'] : 'No traces',
-      genericName: json ['status'] ==1&& json['product']['generic_name'] != '' ? json['product']['generic_name'] : '',
-      nutritionGrade:  json ['status'] ==1?"Notation : ${json['product']['nutrition_grades']}":"Grade Unkwonwn",
-      labels:  json ['status']==1 &&json['product']['labels'] != '' ? json['product']['labels']:'No labels',
-      brands:  json ['status']==1 &&json['product']['brands'] != '' ? json['product']['brands']:'Brands Unknown',
-      origins: json ['status']==1 && json['product']['origins']!= '' ?json['product']['origins']:'Origin Unknows',
+      productName: json ['status'] ==1&& json['product']['product_name'] != '' ?json['product']['product_name']:'-',
+      ingredients: json ['status']==1 && json['product']['ingredients_text'] != '' ? json['product']['ingredients_text']:'-',
+      allergens: json ['status']==1 && json['product']['allergens'] != '' ? json['product']['allergens']:'-',
+      traces:  json ['status']==1 && json['product']['traces'] != '' ? json['product']['traces'] : '-',
+      genericName: json ['status'] ==1&& json['product']['generic_name'] != '' ? json['product']['generic_name'] : '-',
+      nutritionGrade:  json ['status'] ==1?  json['product']['nutrition_grades']:"-",
+      labels:  json ['status']==1 &&json['product']['labels'] != '' ? json['product']['labels']:'-',
+      brands:  json ['status']==1 &&json['product']['brands'] != '' ? json['product']['brands']:'-',
+      origins: json ['status']==1 && json['product']['origins']!= '' ?json['product']['origins']:'-',
       found : json ['status'] == 1
 
     );
@@ -69,15 +69,15 @@ class Post {
   factory Post.fromJsonByName(Map<String, dynamic> json) {
     if (json['products'].isNotEmpty){
     return Post(
-        productName: json['products'][0]['product_name'] != '' ?json['products'][0]['product_name']:'',
-        ingredients: json['products'][0]['ingredients_text'] != '' ? json['products'][0]['ingredients_text']:'Ingredients Unknown',
-        allergens:json['products'][0]['allergens'] != '' ? json['products'][0]['allergens']:'No allergens',
-        traces:  json['products'][0]['traces'] != '' ? json['products'][0]['traces'] : 'No traces',
-        genericName: json['products'][0]['generic_name'] != '' ? json['products'][0]['generic_name'] : '',
-        nutritionGrade:  "Notation : ${json['products'][0]['nutrition_grades']}",
-        labels:  json['products'][0]['labels'] != '' ? json['products'][0]['labels']:'No labels',
-        brands:  json['products'][0]['brands'] != '' ? json['products'][0]['brands']:'Brand Unknwown',
-        origins:json['products'][0]['origins']!= '' ?json['products'][0]['origins']:'Origin Unknwon',
+        productName: json['products'][0]['product_name'] != '' ?json['products'][0]['product_name']:'-',
+        ingredients: json['products'][0]['ingredients_text'] != '' ? json['products'][0]['ingredients_text']:'-',
+        allergens:json['products'][0]['allergens'] != '' ? json['products'][0]['allergens']:'-',
+        traces:  json['products'][0]['traces'] != '' ? json['products'][0]['traces'] : '-',
+        genericName: json['products'][0]['generic_name'] != '' ? json['products'][0]['generic_name'] : '-',
+        nutritionGrade:  json['products'][0]['nutrition_grades'] != '' ? json['products'][0]['nutrition_grades'] : '-',
+        labels:  json['products'][0]['labels'] != '' ? json['products'][0]['labels']:'-',
+        brands:  json['products'][0]['brands'] != '' ? json['products'][0]['brands']:'-',
+        origins:json['products'][0]['origins']!= '' ?json['products'][0]['origins']:'-',
         found : true
 
     );
@@ -99,8 +99,15 @@ class Post {
     }
 }
 
-class ProductDetails extends StatelessWidget {
+// Define a custom Form widget.
+class ProductDetails extends StatefulWidget {
+  @override
+  _ProductDetailsFormState createState() => _ProductDetailsFormState();
+}
 
+class _ProductDetailsFormState extends State<ProductDetails> {
+  bool _allData = false;
+  double _height = 480;
 
   @override
   Widget build(BuildContext context) {
@@ -113,9 +120,7 @@ class ProductDetails extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Product Details'),
-        ),
+
         body: Center(
           child: FutureBuilder<Post>(
             future: post,
@@ -123,31 +128,164 @@ class ProductDetails extends StatelessWidget {
               if (snapshot.hasData) {
                 if(snapshot.data.found) {
                   return
-                    ListView(
+                    Column(
                       children: <Widget>[
-                        ListTile(
-                          title: Text(snapshot.data.productName),
-                        ), ListTile(
-                          title: Text(snapshot.data.nutritionGrade),
-                        ),
-                        ListTile(
-                          title: Text(snapshot.data.genericName),
-                        ),
-                        ListTile(
-                          title: Text(snapshot.data.allergens),
-                        ), ListTile(
-                          title: Text(snapshot.data.traces),
-                        ), ListTile(
-                          title: Text(snapshot.data.labels),
-                        ), ListTile(
-                          title: Text(snapshot.data.brands),
-                        ), ListTile(
-                          title: Text(snapshot.data.origins),
-                        ), ListTile(
-                          title: Text(snapshot.data.ingredients),
+                        Image.asset('images/cristalline.jpg',width : 600,height: _height,fit: BoxFit.cover),
+                        Row(
+                          children: <Widget>[
+                            Text("INFORMATIONS DU PRODUIT",
+                                style: TextStyle(color: Colors.blueAccent, fontSize: 22)
+                            ),
+                            Padding(
+                                padding: EdgeInsets.only(left: 46.0),
+                                child: GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      _allData = !_allData;
+
+                                      if(_allData){
+                                        _height = 50;
+                                      }
+                                      else{
+                                        _height = 480;
+                                      }
+                                    });
+                                  },
+                                  child: Icon(_allData ? Icons.arrow_downward: Icons.arrow_upward),
+                                ),
+
+                            ),
+                          ],
                         )
+                        ,
+
+
+                        ListTile(
+                          title: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text("Nom du produit", style: TextStyle(
+                                  fontWeight: FontWeight.bold),
+                              ),
+                              Padding(
+                                  padding: EdgeInsets.only(left: 26.0),
+                                  child: Text(snapshot.data.productName)
+                              ),
+                            ],
+                          ),
+                        ),
+
+                         ListTile(
+                          title: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text("Nutrition", style: TextStyle(
+                                  fontWeight: FontWeight.bold),
+                              ),
+                           Padding(
+                             padding: EdgeInsets.only(left: 73.0),
+                             child:Text(snapshot.data.nutritionGrade.toUpperCase()),
+                           ),
+                            ],
+                          ),
+                        ),ListTile(
+                      title: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text("Ingrédients", style: TextStyle(
+                              fontWeight: FontWeight.bold),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 54.0),
+                            child:Text(snapshot.data.ingredients),
+                          ),                        ],
+                      ),
+
+                    ),
+
+                ListTile(
+                      title: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text("Nom générique", style: TextStyle(
+                              fontWeight: FontWeight.bold),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 24.0),
+                            child:Text(snapshot.data.genericName),
+                          ),
+                        ],
+                      ),
+                    ),ListTile(
+                      title: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text("Marque", style: TextStyle(
+                              fontWeight: FontWeight.bold),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 81.0),
+                            child:Text(snapshot.data.brands),
+                          ),
+                        ],
+                      ),
+                    ),ListTile(
+                      title: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text("Origine", style: TextStyle(
+                              fontWeight: FontWeight.bold),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 84.0),
+                            child:Text(snapshot.data.origins),
+                          ),
+                        ],
+                      ),
+                    ),ListTile(
+                      title: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text("Labels", style: TextStyle(
+                              fontWeight: FontWeight.bold),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 90.0),
+                            child:Text(snapshot.data.labels),
+                          ),
+                        ],
+                      ),
+                    ),ListTile(
+                      title: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text("Traces", style: TextStyle(
+                              fontWeight: FontWeight.bold),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 88.0),
+                            child:Text(snapshot.data.traces),
+                          ),
+                        ],
+                      ),
+                    ),ListTile(
+                      title: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text("Allergenes", style: TextStyle(
+                              fontWeight: FontWeight.bold),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 58.0),
+                            child:Text(snapshot.data.allergens),
+                          ),
+                        ],
+                      ),
+                    ),
+
                       ],
                     );
+
                 }
               else
                 return Text("Product no found");
